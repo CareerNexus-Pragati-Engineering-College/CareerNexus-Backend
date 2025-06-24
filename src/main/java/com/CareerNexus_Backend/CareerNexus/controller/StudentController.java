57c57c358275758f624d10f21e0917dc89acf245
SAI TEJA VENKATA VINAY ADAPA <144699556+22A31A0525@users.noreply.github.com>
2025-06-24 16:51:08 +0530

    Update StudentController.java

--- src/main/java/com/CareerNexus_Backend/CareerNexus/controller/StudentController.java
+++ src/main/java/com/CareerNexus_Backend/CareerNexus/controller/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/auth/students")
+@RequestMapping("/auth/student")
 public class StudentController {
 
     private final StudentService studentService;
